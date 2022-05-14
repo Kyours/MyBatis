a73b209da980ab5b97f2da55c502473decd2476b
Kyours <75402999+Kyours@users.noreply.github.com>
2022-05-15 02:27:03 +0800

    pull commit

--- MyBatis_MBG/src/test/java/com/atguigu/mybatis/TestMBG.java
+++ MyBatis_MBG/src/test/java/com/atguigu/mybatis/TestMBG.java
@@ -43,5 +43,6 @@ public class TestMBG {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
