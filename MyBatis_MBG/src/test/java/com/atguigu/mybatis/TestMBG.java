a6db21c11ab4fd97fbd4455789fd02c9f52487c1
Kyours <ma121254099@163.com>
2022-05-15 01:33:28 +0800

    hot-fix commit2

--- MyBatis_MBG/src/test/java/com/atguigu/mybatis/TestMBG.java
+++ MyBatis_MBG/src/test/java/com/atguigu/mybatis/TestMBG.java
@@ -40,5 +40,6 @@ public class TestMBG {
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
+        System.out.println("hot-fix test");
     }
 }
